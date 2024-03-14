88ebe7eb644da3f20f3031b47160e309443d6a9e
My Nguyen <chiffonng136@gmail.com>
2024-03-14 08:18:00 +0100

    add working components

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,62 @@
+import { Link, useNavigate } from 'react-router-dom';
+import React, { useContext } from 'react';
+import UserContext from '../UserContext';
+import axios from 'axios';
+
+export default function Header() {
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+
+  const handleLogout = () => {
+    // Remove the token from localStorage
+    localStorage.removeItem('jwtToken');
+    localStorage.removeItem('userEmail');
+
+    // Clear the JWT from Axios headers
+    delete axios.defaults.headers.common['Authorization'];
+
+    // Clear user from React context
+    setUser(null);
+    navigate('/login');
+  };
+
+  return (
+    <header className="bg-white">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <div className="-m-1.5 p-1.5">
+            <h1 className="text-2xl font-bold mt-2">ToDo</h1>
+          </div>
+        </div>
+
+        <div className="flex flex-1 items-center justify-end gap-x-6">
+
+          {/*
+            if a user exits, show the user's email and a logout button
+            if not, show a login and signup button
+          */}
+          {user ? (
+            <>
+              <span className="text-gray-700">{user.email}</span>
+              <button
+                onClick={handleLogout}
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <Link to="/login"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Log in
+            </Link>
+          )}
+        </div>
+
+
+      </nav>
+    </header>
+  )
+};
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    colors: {
-      'blue': '#3060BF',
-      'red': '#F94776',
-      'yellow': '#F6D54B',
-      'green': '#4CAF50'
-    },
-    extend: {
-      fontFamily: {
-        sans: ['Inter var'],
-      },
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms')
+  theme: [
+    'cupcake',
+    'dracula'
   ],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('daisyui')
+  ],
+  daisyui: {
+    themes: true, // enable color themes
+    darkTheme: "dracula", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 }
 
